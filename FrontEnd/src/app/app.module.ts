798fbb1eba826f652cc161c3461d50506905e5b1
MedhaPujari <tmedha.05@gmail.com>
2018-01-18 22:49:11 -0500

    Sign Up and Login pages

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -7,28 +7,16 @@ import { SignupComponent } from './signup/signup.component';
 import {LoginComponent} from './login/login.component';
 import {HomeComponent} from './home/home.component';
 import {RouterModule, Routes} from '@angular/router';
-import { Link1Component } from './link1/link1.component';
-import { Link2Component } from './link2/link2.component';
-import { Link3Component } from './link3/link3.component';
-import { Component } from '@angular/core/src/metadata/directives';
+
+
+
 
 export const appRoutes: Routes=[
-  {path: 'Signup',
-  children:[
-  {path: '',redirectTo:'Link1',pathMatch:'full'},
-    {path: 'Link1',component:Link1Component},
-    {path: 'Link2',component:Link2Component},
-    {path: 'Link3',component:Link3Component}    
-  ], 
-   component: SignupComponent},
-  {path: 'Login',
-  children:[
-    {path: '',redirectTo:'Link2',pathMatch:'full'},
-        {path: 'Link1',component:Link1Component},
-        {path: 'Link2',component:Link2Component},
-        {path: 'Link3',component:Link3Component}    
-      ], 
-   component: LoginComponent},
+  {path: '',redirectTo:'Home',pathMatch:'full'},
+  {path: 'Signup',component: SignupComponent},
+  {path: 'Login',component: LoginComponent},
+  {path: 'Home',component: HomeComponent}
+   
   
 ];
 
@@ -38,10 +26,7 @@ export const appRoutes: Routes=[
     SignupComponent,
     LoginComponent,
     HomeComponent,
-    Link1Component,
-    Link2Component,
-    Link3Component
-  ],
+    ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes)
