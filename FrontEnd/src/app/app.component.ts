e4c7f1e175d7b4ddf114e76051e2893e543717f4
sannaini <santhoshnaini40@gmail.com>
2018-01-23 14:11:04 +0530

    getting latest code

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'mywish-static-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Medha';
-}
+export class AppComponent {}
--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,26 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 import {LoginComponent} from './login/login.component';
 import {HomeComponent} from './home/home.component';
 import {RouterModule, Routes} from '@angular/router';
-
-
-
-
 export const appRoutes: Routes=[
   {path: '',redirectTo:'Home',pathMatch:'full'},
   {path: 'Signup',component: SignupComponent},
   {path: 'Login',component: LoginComponent},
   {path: 'Home',component: HomeComponent}
-   
-  
 ];
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -36,4 +27,5 @@ export const appRoutes: Routes=[
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+}
